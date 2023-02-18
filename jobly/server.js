da479354b5cb6007eb80f24b8941066f04c17bc6
root <root@LAPTOP-EEV6NRCI>
2023-02-18 13:24:33 -0600

    ..

--- jobly/server.js
+++ jobly/server.js
@@ -3,10 +3,6 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
-try{
 app.listen(PORT, function () {
   console.log(`Started on http://localhost:${PORT}`);
 });
-}catch(error){
-  console.error(`Seems there was an error starting the server: ${error}`)
-}
